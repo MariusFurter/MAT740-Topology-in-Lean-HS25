5ac7e9f9761d0b370a7eccaca30ce66786dec4b3
MariusFurter <80038150+MariusFurter@users.noreply.github.com>
2025-11-03 14:32:16 +0100

    week 8

--- MAT740TopologyInLeanHS25/Definitions/NewSpaces.lean
+++ MAT740TopologyInLeanHS25/Definitions/NewSpaces.lean
@@ -106,7 +106,20 @@ instance Subspace_pullbackTopology {S : Type u} (incl : S → X) (inj : Function
     incl := incl
     Injective_incl := inj
     char_Subspace := by
-      sorry -- exercise
+      intro T TT f
+      constructor
+      case mp =>
+        intro cont_f V open_V
+        rw [Set.preimage_comp]
+        apply cont_f
+        use V
+      case mpr =>
+        intro cont_if U open_U
+        obtain ⟨V, hV1, hV2⟩ := open_U
+        rw [hV2]
+        rw [← Set.preimage_comp]
+        apply cont_if
+        exact hV1
 
 /- # Quotient Spaces -/
 
@@ -119,23 +132,35 @@ class Quotient (X : Type u) [TX : Topology X] where
   char_Quotient {T : Type u} (TT: Topology T) (f : Q → T) : Cont f ↔ Cont (f ∘ qmap)
 
 theorem Cont_qmap [quot : Quotient X] : @Cont X quot.Q TX quot.TQ quot.qmap := by
-  sorry -- exercise
+    have h : id ∘ quot.qmap = quot.qmap := by
+      exact rfl
+    rw [← h]
+    rw [← quot.char_Quotient]
+    intro U open_U
+    exact open_U
 
 /- The quotient topology is the largest (finest) topology on Q that makes `qmap` continuous. -/
 theorem final_Quotient [quot : Quotient X] [TQ' : Topology quot.Q] :
   @Cont X quot.Q TX TQ' quot.qmap → TQ' ≤ quot.TQ := by
-    sorry -- exercise
+    intro h
+    rw [Coarser]
+    rw [quot.char_Quotient, Function.id_comp]
+    exact h
 
 instance pushforwardTopology
   (X : Type u) (TX : Topology X) (Q : Type u) (qmap : X → Q)
   : Topology Q where
     Open := fun (U : Set Q) ↦ Open (qmap ⁻¹' U)
     Open_univ := by
-      sorry
+      simp only [Set.preimage_univ, Open_univ]
     Open_inter := by
-      sorry
+      intro U V hU hV
+      rw [Set.preimage_inter]
+      exact Open_inter hU hV
     Open_sUnion := by
-      sorry -- hint : Open_preimageUnion
+      intro C hC
+      rw [Set.preimage_sUnion]
+      exact Open_preimageUnion hC
 
 instance Quotient_pushforwardTopology
   {Q : Type u} (qmap : X → Q) (surj : Function.Surjective qmap)
@@ -145,6 +170,95 @@ instance Quotient_pushforwardTopology
     qmap := qmap
     Surjective_qmap := surj
     char_Quotient := by
-      sorry --exercise
+      intro T TT f
+      constructor
+      case mp =>
+        intro cont_f U open_U
+        apply cont_f
+        exact open_U
+      case mpr =>
+        intro cont_fq U open_U
+        exact cont_fq U open_U
+
+/- # Product Spaces -/
+
+class ProductSpace (X : Type u) [Topology X] (Y : Type u) [Topology Y] where
+  TP : Topology (X × Y)
+  /- Top(T,P) ≅ Top(T,X) × Top(T,Y) -/
+  char_Product {T : Type u} (TT : Topology T) (f : T → X × Y) :
+    Cont f ↔ Cont (Prod.fst ∘ f) ∧ Cont (Prod.snd ∘ f)
+
+class iProductSpace (I : Type u) (Xs : I → Type u) (TXs : (i : I) → Topology (Xs i)) where
+  TP : Topology (Π i, Xs i) -- (\P) dependent product
+  /- Top(T, Π X i) ≅ Π Top(T,X i) -/
+  char_Product {T : Type u} (TT : Topology T) (f : T → (Π i, Xs i))
+    : Cont f ↔ ∀ i, Cont (fun t ↦ f t i)
+
+-- Projections are continuous
+theorem Cont_fst
+  (X : Type u) [TX : Topology X] (Y : Type u) [TY : Topology Y] [prod : ProductSpace X Y]
+  : (@Cont (X × Y) X prod.TP TX Prod.fst) := by sorry
+
+theorem Cont_snd
+  (X : Type u) [TX : Topology X] (Y : Type u) [TY : Topology Y] [prod : ProductSpace X Y]
+  : (@Cont (X × Y) Y prod.TP TY Prod.snd) := by sorry
+
+theorem Cont_iproj
+  {I : Type u} {Xs : I → Type u} {TXs : (i : I) → Topology (Xs i)} [prod : iProductSpace I Xs TXs]
+  : ∀ i : I, @Cont (Π i, Xs i) (Xs i) prod.TP (TXs i) (fun x ↦ x i) := by sorry
+
+-- Product topology is coarsest topology making projections continuous.
+
+-- Projections are open maps
+
+instance productBasis
+  (X : Type u) [Topology X] (Y : Type u) [Topology Y]
+  : Basis (X × Y) where
+    /- Basis of the form U × V for U open in X, V open in Y.
+    Note the use of `Set.prod`.
+    -/
+    Basics :=
+      {B : Set (X × Y) | ∃ (BX : Set X) (BY : Set Y), Open BX ∧ Open BY ∧ B = Set.prod BX BY}
+    Basis_cover := by sorry
+    Basis_inter := by sorry
+
+instance productTopology (X : Type u) [TX : Topology X] (Y : Type u) [TY : Topology Y]
+  : Topology (X × Y) := @basisTopology (X × Y) (productBasis X Y)
+
+instance Product_productTopology (X : Type u) [TX : Topology X] (Y : Type u) [TY : Topology Y] : ProductSpace X Y where
+  TP := productTopology X Y
+  char_Product := by
+    sorry
+
+
+/- # Coproduct Spaces -/
+
+class CoproductSpace (X : Type u) [Topology X] (Y : Type u) [Topology Y] where
+  TC : Topology (X ⊕ Y)
+  /- Top(X ⊕ Y,T) ≅ Top(X,T) × Top(Y,T) -/
+  char_Coproduct {T : Type max u} (TT : Topology T) (f : (X ⊕ Y) → T) :
+    Cont f ↔ Cont (f ∘ Sum.inl) ∧ Cont (f ∘ Sum.inr)
+
+class iCoproductSpace (I : Type u) (Xs : I → Type u) (TXs : I → Topology X) where
+  TC : Topology (Σ i , Xs i)  -- (\S) dependent sum
+  /- Top(Σ X i,T) ≅ Π Top(X i, T) -/
+  char_Product {T : Type max u} (TT : Topology T) (f : (Σ i , Xs i) → T) :
+    Cont f ↔ ∀ i, Cont (fun x ↦ f ⟨i, x⟩)
+
+-- Injections are continuous
+
+-- Coproduct topology is finest topology making projections continuous.
+
+-- Injections are open and closed maps
+
+instance coproductTopology (X : Type u) [TX : Topology X] (Y : Type u) [TY : Topology Y] : Topology (X ⊕ Y) where
+  Open := {B | Open (Sum.inl ⁻¹' B) ∧ Open (Sum.inr ⁻¹' B)}
+  Open_univ := by sorry
+  Open_inter := by sorry
+  Open_sUnion := by sorry
+
+instance Coproduct_coproductTopology (X : Type u) [TX : Topology X] (Y : Type u) [TY : Topology Y] : CoproductSpace X Y where
+  TC := coproductTopology X Y
+  char_Coproduct := by sorry
 
 end Constructions
